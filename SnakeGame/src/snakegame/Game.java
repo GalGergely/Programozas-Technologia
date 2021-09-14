797da91ac1e615c70405b7c48b5e594746b13cd1
GalGergely <galgergely252@gmail.com>
2021-09-14 08:56:09 +0200

    snake

---
+++ SnakeGame/src/snakegame/Game.java
@@ -0,0 +1,207 @@
+package snakegame;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.GraphicsEnvironment;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class Game extends JPanel implements ActionListener{
+    static final int GAME_WIDTH = 600;
+    static final int GAME_HEIGHT = 600;
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNITS = (GAME_WIDTH*GAME_HEIGHT)/UNIT_SIZE;
+    static final int DELAY = 70;
+    final int x[] = new int[GAME_UNITS];
+    final int y[] = new int[GAME_UNITS];
+    int bodyParts=1;
+    int applesEaten=0;
+    int appleX;
+    int appleY;
+    char direction='R';
+    boolean running=false;
+    Timer timer;
+    Random random;
+    Font retroFont1;
+    Font retroFont2;
+ 
+ Game(){
+  try{
+               
+        retroFont1 = Font.createFont(Font.TRUETYPE_FONT, new File("src/retro.ttf")).deriveFont(120f);	
+            GraphicsEnvironment ge1 = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge1.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("src/retro.ttf")));
+        retroFont2 = Font.createFont(Font.TRUETYPE_FONT, new File("src/retro.ttf")).deriveFont(50f);	
+            GraphicsEnvironment ge2 = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge2.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("src/retro.ttf")));
+    }
+	catch(IOException | FontFormatException e){
+			
+    }
+        random = new Random();
+        this.setBackground(Color.DARK_GRAY);
+        this.setFocusable(true);
+        this.addKeyListener(new Game.MyKeyAdapter());
+        startGame();
+        this.setSize(600, 600);
+        this.setLocation(100,0);
+  
+ }
+public void startGame(){
+        newApple();
+        running=true;
+        timer=new Timer(DELAY,this);
+        timer.start();
+    }
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        draw(g);
+    }
+    public void draw(Graphics g){
+        if(running)
+        {
+        g.setColor(Color.gray);
+        g.setPaintMode();
+        for(int i=0; i<GAME_HEIGHT/UNIT_SIZE; i++)
+        {
+            g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, GAME_HEIGHT);
+            g.drawLine(0, i*UNIT_SIZE, GAME_WIDTH, i*UNIT_SIZE);
+        }
+        g.setColor(Color.RED);
+        g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+        for(int i=0; i<bodyParts; i++)
+        {
+            if(i==0)
+            {
+                g.setColor(new Color(0, 255, 255));
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }
+            else
+            {
+                if((i*50)<255){g.setColor(new Color((i*50), 255, 255));}
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }
+        }
+        g.setColor(Color.red);
+	g.setFont(retroFont2);
+	FontMetrics metrics = getFontMetrics(g.getFont());
+	g.drawString("Score    "+applesEaten, (GAME_WIDTH - metrics.stringWidth("Score "+applesEaten))/2, 50);
+        }
+        else
+        {
+            gameOver(g);
+        } 
+    }
+    public void move(){
+        for(int i=bodyParts; i>0; i--)
+        {
+            x[i] = x[i-1];
+            y[i] = y[i-1];
+        }
+        switch(direction)
+        {
+            case 'U': y[0] = y[0] - UNIT_SIZE; break;
+            case 'D': y[0] = y[0] + UNIT_SIZE; break;
+            case 'L': x[0] = x[0] - UNIT_SIZE; break;
+            case 'R': x[0] = x[0] + UNIT_SIZE; break;
+        }  
+    }
+    public void newApple(){
+        appleX = random.nextInt((int)(GAME_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+        appleY = random.nextInt((int)(GAME_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
+        
+    }
+public void checkApple() {
+		if((x[0] == appleX) && (y[0] == appleY)) 
+                {
+			bodyParts++;
+			applesEaten++;
+			newApple();
+		}
+	}
+    public void checkCollisions(){
+        for(int i=bodyParts; i>0; i--)
+        {
+            if((x[0]== x[i])&& (y[0] == y[i]))
+            {
+                running = false;
+            } 
+        }
+        if(x[0]<0)
+        {
+            running = false;
+        }
+        if(x[0]>=GAME_WIDTH)
+        {
+            running = false;
+        }
+        if(y[0]<0)
+        {
+            running = false;
+        }
+        if(y[0]>=GAME_HEIGHT)
+        {
+            running = false;
+        }
+        if(!running)
+        {
+            timer.stop();
+        }
+    }
+    public void gameOver(Graphics g) {
+        
+		g.setColor(Color.red);
+		g.setFont(retroFont2);
+		FontMetrics metrics1 = getFontMetrics(g.getFont());
+		g.drawString("Score   "+applesEaten, (GAME_WIDTH - metrics1.stringWidth("Score: "+applesEaten))/2, 100);
+
+		g.setColor(Color.red);
+		g.setFont(retroFont1);
+		FontMetrics metrics2 = getFontMetrics(g.getFont());
+		g.drawString("Game Over", (GAME_WIDTH - metrics2.stringWidth("Game Over"))/2, 300);
+	}
+    @Override
+  	public void actionPerformed(ActionEvent e) {
+		if(running) 
+                {
+                    move();
+                    checkApple();
+                    checkCollisions();
+		}
+		repaint();
+	}
+        public class MyKeyAdapter extends KeyAdapter{
+        
+        @Override
+        public void keyPressed(KeyEvent e){
+            switch(e.getKeyCode())
+            {
+                case KeyEvent.VK_A: if(direction != 'R'){direction = 'L';}break;
+                case KeyEvent.VK_D: if(direction != 'L'){direction = 'R';}break;
+                case KeyEvent.VK_W: if(direction != 'D'){direction = 'U';}break;
+                case KeyEvent.VK_S: if(direction != 'U'){direction = 'D';}break;
+                case KeyEvent.VK_SPACE: 
+                    if(timer.isRunning())
+                    {
+                         timer.stop();
+                    }
+                    else
+                    {
+                        timer.start();
+                    }
+                    break;
+
+            }
+        }
+    }
+}
---
+++ SnakeGame/src/snakegame/GameFrame.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snakegame;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class GameFrame extends JFrame implements ActionListener{
+
+ Game game;
+ JButton resetButton;
+ 
+ GameFrame(){
+  this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  this.setSize(715, 640);
+  this.setLayout(null);
+  resetButton = new JButton();
+  resetButton.setText("Reset");
+  resetButton.setSize(100, 50);
+  resetButton.setLocation(0, 200);
+  resetButton.addActionListener(this);
+  game = new Game();
+  this.add(resetButton);
+  this.add(game);
+  this.setVisible(true);
+ }
+
+ @Override
+ public void actionPerformed(ActionEvent e) {
+  if(e.getSource()==resetButton) 
+  {
+   this.remove(game);
+   game = new Game();
+   this.add(game);
+  }
+ }
+}
